dcddb965dc89a304240bc25ca7fbf23272b157ab
canh-duong-0804 <141054046+canh-duong-0804@users.noreply.github.com>
2024-12-05 00:08:04 +0700

    landingpage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // ** React Imports
-import { Suspense, lazy } from 'react'
+import { Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
 
-// ** Intl, CASL & ThemeColors Context
+// ** Intl & ThemeColors Context
 import ability from './configs/acl/ability'
 import { AbilityContext } from './utility/context/Can'
 import { ThemeContext } from './utility/context/ThemeColors'
@@ -18,39 +18,16 @@ import themeConfig from './configs/themeConfig'
 // ** Toast
 import { Toaster } from 'react-hot-toast'
 
-// ** i18n
-import './configs/i18n'
+// ** Component
+import App from './App'
 
-// ** Spinner (Splash Screen)
+// ** Spinner
 import Spinner from './@core/components/spinner/Fallback-spinner'
 
-// ** Ripple Button
-import './@core/components/ripple-button'
-
-// ** Fake Database
-import './@fake-db'
-
-// ** PrismJS
-import 'prismjs'
-import 'prismjs/themes/prism-tomorrow.css'
-import 'prismjs/components/prism-jsx.min'
-
-// ** React Perfect Scrollbar
-import 'react-perfect-scrollbar/dist/css/styles.css'
-
-// ** React Hot Toast Styles
-import '@styles/react/libs/react-hot-toasts/react-hot-toasts.scss'
-
-// ** Core styles
-import './@core/assets/fonts/feather/iconfont.css'
+// ** Styles
+import '@styles/base/pages/authentication.scss'
 import './@core/scss/core.scss'
 import './assets/scss/style.scss'
-
-// ** Service Worker
-import * as serviceWorker from './serviceWorker'
-
-// ** Lazy load app
-const LazyApp = lazy(() => import('./App'))
 
 const container = document.getElementById('root')
 const root = createRoot(container)
@@ -61,16 +38,11 @@ root.render(
       <Suspense fallback={<Spinner />}>
         <AbilityContext.Provider value={ability}>
           <ThemeContext>
-            <LazyApp />
-            <Toaster position={themeConfig.layout.toastPosition} toastOptions={{ className: 'react-hot-toast' }} />
+            <App />
+            <Toaster position={themeConfig.layout.toastPosition} />
           </ThemeContext>
         </AbilityContext.Provider>
       </Suspense>
     </Provider>
   </BrowserRouter>
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
